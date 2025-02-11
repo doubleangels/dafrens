5a37070c146ffe744617b396c29e756a01d0d406
doubleangels <matt@doubleangels.com>
2025-02-10 22:43:05 -0700

    Remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { compression } from 'vite-plugin-compression2'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
